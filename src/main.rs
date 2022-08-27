7375ca95997e46b3b8a8e386a94dd6df3ecc8de5
Marc S. Brooks <marc@mbrooks.info>
2022-08-27 09:19:35 -0700

    Added OS-specific dependency check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 // Port of https://github.com/nuxy/MiscreatedMods-DOA/blob/master/publish.sh
 
+use std::env;
+use std::process;
+
+// Load cargo.
 use clap::{AppSettings, Parser};
+use which::which;
 
 const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 
@@ -23,10 +28,34 @@ struct Cli {
 }
 
 fn main() {
+    check_deps();
+
     let args = Cli::parse();
 
     println!("Username: {:?}", args.username);
     println!("Password: {:?}", args.password);
     println!("Workshop: {:?}", args.workshop);
     println!("Public:   {:?}", args.public);
+}
+
+/**
+ * Check OS-specific dependencies.
+ */
+fn check_deps() {
+    let file_ext: &str = if env::consts::OS == "windows" {
+        ".exe"
+    } else {
+        ""
+    };
+
+    let file_names = &["steamcmd", "git", "7za"];
+
+    for file_name in file_names {
+        let result: &str = which(file_name).unwrap().as_path().display().to_string();
+
+        if result == "" {
+            println!("{file_name}{file_ext} is not installed. Exiting.");
+            process::exit(1);
+        }
+    }
 }
