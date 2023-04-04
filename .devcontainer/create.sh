c9da23b8cdbcd67cfe0fccee4114e606dff8e898
Marc S. Brooks <marc@mbrooks.info>
2023-04-04 10:33:05 -0700

    Run Wine as root (Steam login fix)

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -8,10 +8,13 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Wrap SteamCMD binary (use Wine)
 cat << EOF > /usr/bin/steamcmd
 #!/bin/sh
-sudo -u games wine /usr/games/Steam/steamcmd.exe "\$@"
+wine /usr/games/Steam/steamcmd.exe "\$@"
 EOF
 
 chmod 755 /usr/bin/steamcmd
 
 # Suppress ALSA lib warnings.
-mkdir /usr/games/.config/pulse && chown games:games /usr/games/.config/pulse
+mkdir /usr/games/.config/pulse
+
+# Enable root access (Steam required).
+chown -R root:root /usr/games/.wine
